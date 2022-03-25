6dd274edb6e0b4ad030ace460abc1e4852b496aa
Himanshu <venom.himanshu@gmail.com>
2022-03-25 23:33:17 +0530

    failed method commited

--- src/test/java/demoTestNg/testngdemo/App2.java
+++ src/test/java/demoTestNg/testngdemo/App2.java
@@ -1,5 +1,15 @@
 package demoTestNg.testngdemo;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class App2 {
@@ -8,5 +18,30 @@ public class App2 {
   @Test
   public void method() {
 	  System.out.println("this is test");
-  }
+}
+	
+	public static String browser = "chrome";
+	public static WebDriver driver;
+	
+	@Test
+	public  void driversetup() {
+		if (browser.equals("chrome")) {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+	}
+
+	@Test(dependsOnMethods ={"driversetup"})
+	public void geturl() {
+		driver.get("www.facbook.com");
+	}
+	
+	@Test(dependsOnMethods ={"geturl"})
+	
+	public void gettitle(){
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+	
 }
